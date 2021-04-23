75b590c912a9b00b3076b415d9885392f6f4a982
naza <nazari_93@hotmail.com>
2021-04-23 14:45:31 +0200

    Proyecto final

---
+++ NuGetModels/Models/Mascota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NuGetModels.Models
+{
+    [Table("MASCOTAS")]
+    public class Mascota
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("NOMBRE")]
+        public String Nombre { get; set; }
+        [Column("RAZA")]
+        public String Raza { get; set; }
+        [Column("SEXO")]
+        public String Sexo { get; set; }
+        [Column("EDAD")]
+        public int Edad { get; set; }
+    }
+}
---
+++ NuGetModels/Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NuGetModels.Models
+{
+    [Table("PERSONAS")]
+    public class Persona
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("NOMBRE")]
+        public String Nombre { get; set; }
+        [Column("APELLIDOS")]
+        public String Apellidos { get; set; }
+        [Column("DIRECCION")]
+        public String Direccion { get; set; }
+        [Column("EDAD")]
+        public int Edad { get; set; }
+    }
+}
